a9f3c079286fc8e68c588034f7dec53d50c7cf38
Rémy JUANES <rjuanes@cae.com>
2020-06-02 13:58:35 -0400

    add commander dependency and begin to work with it

---
+++ bin/index.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+"use strict";
+console.log("you are in CLI mode");
+const {program} = require('commander');
+program.version('0.0.1');
+
+
+program.command("add <filePath>")
+    .description("add a file or folder to the config file")
+    .action((filePath) => {
+        console.log("add param filePath:", filePath);
+    });
+
+program.command("remove <filePath>")
+    .description("remove a file or folder to the config file")
+    .action((filePath) => {
+        console.log("remove param filePath:", filePath);
+    });
+
+program.command("cpzs")
+    .description("do its job")
+    .action(() => {
+        console.log("Let's the hack begin");
+    });
+
+program.parse(process.argv);
