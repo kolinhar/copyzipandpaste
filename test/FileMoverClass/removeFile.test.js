d37081137f835f9939b7542ddd3b9f114cd908f3
Rémy JUANES <rjuanes@sepro-group.com>
2022-09-08 15:51:08 +0200

    refactor removeFile tests and add a test

--- test/FileMoverClass/removeFile.test.js
+++ test/FileMoverClass/removeFile.test.js
@@ -4,32 +4,44 @@ const path = require('path');
 const { FileMover } = require('../../libs/FileMover');
 
 const filePathUnexisting = `${__dirname}${path.sep}someUnexistingFile.nope`;
-
 const fileDestExisting = `${__dirname}${path.sep}test-copy.js`;
 
 fs.writeFileSync(fileDestExisting, 'Blablablablabla');
 
 exports.removeFile = describe('FileMover.removeFile', () => {
   it("should reject Promise because file doesn't exist", () => {
-    assert.rejects(FileMover.removeFile(filePathUnexisting), {
-      message: `ENOENT: no such file or directory, unlink '${filePathUnexisting}'`,
-    });
+    assert.rejects(FileMover.removeFile(filePathUnexisting));
   });
 
-  it('delete a file', (done) => {
+  it('delete a file', () => {
+    assert.doesNotReject(FileMover.removeFile(fileDestExisting));
+  });
+
+  it('check if file is really deleted', (done) => {
+    fs.writeFileSync(fileDestExisting, 'Blablablablabla');
+
     FileMover.removeFile(fileDestExisting)
-      .then(() => {
-        fs.access(fileDestExisting, (err) => {
-          if (err) {
-            assert.ok(true);
-          } else {
-            assert.fail(`file ${fileDestExisting} not deleted`);
-          }
-        });
-      })
+      .then(
+        () => {
+          fs.access(fileDestExisting, (err) => {
+            if (err) {
+              assert.ok(true);
+            } else {
+              assert.fail(`file ${fileDestExisting} not deleted`);
+            }
+          });
+        },
+        (err) => {
+          assert.fail(err);
+        }
+      )
       .catch((err) => {
         assert.fail(err);
       })
       .finally(done);
+  });
+
+  it('try to delete an already deleted file', () => {
+    assert.rejects(FileMover.removeFile(fileDestExisting));
   });
 });
