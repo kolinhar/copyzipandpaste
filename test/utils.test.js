baaca02f1fe67210c6c8d9a338734a7feca2a415
Rémy JUANES <rjuanes@sepro-group.com>
2022-09-29 12:06:05 +0200

    add unit tests

--- test/utils.test.js
+++ test/utils.test.js
@@ -4,15 +4,18 @@ const {
   getCurrentPathFromFilePath,
   checkPath,
   absolutingPath,
+  formatStdoutFromJSONNmprc,
+  formatStdout,
+  formatJSONToNpmrc,
   m10,
 } = require('../src/libs/utils');
 
 describe('getCurrentFolderName', () => {
-  it('should return the last folder of an absolute path', () => {
+  it('should return the last folder of an absolute path 1', () => {
     assert.strictEqual(getCurrentFolderName(__dirname), 'test');
   });
 
-  it('should return the last folder of an absolute path', () => {
+  it('should return the last folder of an absolute path 2', () => {
     assert.strictEqual(
       getCurrentFolderName(
         'C:\\Users\\rjuanes\\Documents\\Dev\\copyzipandpaste\\test\\FileMoverClass'
@@ -21,7 +24,7 @@ describe('getCurrentFolderName', () => {
     );
   });
 
-  it('should return the last folder of an absolute path', () => {
+  it('should return the last folder of an absolute path 3', () => {
     assert.strictEqual(
       getCurrentFolderName(
         'C:\\Users\\rjuanes\\Documents\\Dev\\copyzipandpaste'
@@ -30,7 +33,7 @@ describe('getCurrentFolderName', () => {
     );
   });
 
-  it('should return the last folder of an absolute path', () => {
+  it('should return the last folder of an absolute path 4', () => {
     assert.strictEqual(
       getCurrentFolderName(
         'C:\\Users\\rjuanes\\Documents\\Dev\\copyzipandpaste'
@@ -39,14 +42,14 @@ describe('getCurrentFolderName', () => {
     );
   });
 
-  it('should return false with a relative path', () => {
+  it('should return false with a relative path 1', () => {
     assert.strictEqual(
       getCurrentFolderName('.\\WebstormProjects\\copyzipandpaste'),
       false
     );
   });
 
-  it('should return false with a relative path', () => {
+  it('should return false with a relative path 2', () => {
     assert.strictEqual(
       getCurrentFolderName('..\\WebstormProjects\\copyzipandpaste'),
       false
@@ -55,35 +58,35 @@ describe('getCurrentFolderName', () => {
 });
 
 describe('getCurrentFolderNameFromFilePath', () => {
-  it('should return the absolute path of folder from an absolute file path', () => {
+  it('should return the absolute path of folder from an absolute file path 1', () => {
     assert.strictEqual(
       getCurrentPathFromFilePath(__filename),
       'C:\\Users\\rjuanes\\Documents\\Dev\\copyzipandpaste\\test'
     );
   });
 
-  it('should return the absolute path of folder from an absolute file path', () => {
+  it('should return the absolute path of folder from an absolute file path 2', () => {
     assert.strictEqual(
       getCurrentPathFromFilePath(__filename),
       'C:\\Users\\rjuanes\\Documents\\Dev\\copyzipandpaste\\test'
     );
   });
 
-  it('should return the absolute path of folder from an absolute file path', () => {
+  it('should return the absolute path of folder from an absolute file path 3', () => {
     assert.strictEqual(
       getCurrentPathFromFilePath(__filename),
       'C:\\Users\\rjuanes\\Documents\\Dev\\copyzipandpaste\\test'
     );
   });
 
-  it('should return false with a relative file path', () => {
+  it('should return false with a relative file path 1', () => {
     assert.strictEqual(
       getCurrentPathFromFilePath('.\\WebstormProjects\\copyzipandpaste\\test'),
       false
     );
   });
 
-  it('should return false with a relative file path', () => {
+  it('should return false with a relative file path 2', () => {
     assert.strictEqual(
       getCurrentPathFromFilePath('..\\WebstormProjects\\copyzipandpaste\\test'),
       false
@@ -92,11 +95,11 @@ describe('getCurrentFolderNameFromFilePath', () => {
 });
 
 describe('checkAbsolutePath', () => {
-  it('should works', (done) => {
+  it('should works 1', (done) => {
     assert.doesNotReject(checkPath(__filename)).finally(done);
   });
 
-  it('should works', (done) => {
+  it('should works 2', (done) => {
     assert.doesNotReject(checkPath(__dirname)).finally(done);
   });
 
@@ -140,25 +143,52 @@ describe('absolutingPath', () => {
       'C:\\Users\\rjuanes\\Documents\\Dev\\copyzipandpaste\\FileMoverClass'
     );
   });
+});
 
-  describe('m10', () => {
-    it('shoud return "00"', () => {
-      assert.deepStrictEqual(m10(0), '00');
+describe('formatStdout', () => {
+  const testStr = `undefined\n`;
+  it('remove last character', () => {
+    assert.strictEqual(formatStdout(testStr), 'undefined');
+  });
+});
+
+describe('formatStdoutFromJSONNmprc', () => {
+  it('remove first and last character', () => {
+    assert.deepEqual(formatStdoutFromJSONNmprc(`"{"test": true}"`), {
+      test: true,
     });
-    it('shoud return "01"', () => {
-      assert.deepStrictEqual(m10(1), '01');
-    });
-    it('shoud return "03"', () => {
-      assert.deepStrictEqual(m10(3), '03');
-    });
-    it('shoud return "09"', () => {
-      assert.deepStrictEqual(m10(9), '09');
-    });
-    it('shoud return "10"', () => {
-      assert.deepStrictEqual(m10(10), '10');
-    });
-    it('shoud return "100"', () => {
-      assert.deepStrictEqual(m10(100), '100');
-    });
+  });
+});
+
+describe('formatJSONToNpmrc', () => {
+  it('do some stuff', () => {
+    assert.strictEqual(
+      formatJSONToNpmrc({
+        test: true,
+        path: 'C:\\Users\\rjuanes\\Downloads',
+      }),
+      `"\\"{\\"test\\":true,\\"path\\":\\"C:\\\\Users\\\\rjuanes\\\\Downloads\\"}\\""`
+    );
+  });
+});
+
+describe('m10', () => {
+  it('shoud return "00"', () => {
+    assert.deepStrictEqual(m10(0), '00');
+  });
+  it('shoud return "01"', () => {
+    assert.deepStrictEqual(m10(1), '01');
+  });
+  it('shoud return "03"', () => {
+    assert.deepStrictEqual(m10(3), '03');
+  });
+  it('shoud return "09"', () => {
+    assert.deepStrictEqual(m10(9), '09');
+  });
+  it('shoud return "10"', () => {
+    assert.deepStrictEqual(m10(10), '10');
+  });
+  it('shoud return "100"', () => {
+    assert.deepStrictEqual(m10(100), '100');
   });
 });
